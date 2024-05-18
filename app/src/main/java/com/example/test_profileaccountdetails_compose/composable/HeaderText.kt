6d37b82baab510c7071957754271acf3f0c9690d
AmrNasser <amr931111@gmail.com>
2024-05-18 19:28:02 +0300

    add Header text and  SpacerVertical32 and image

---
+++ app/src/main/java/com/example/test_profileaccountdetails_compose/composable/HeaderText.kt
@@ -0,0 +1,40 @@
+package com.example.test_profileaccountdetails_compose.composable
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.test_profileaccountdetails_compose.ui.theme.PrimaryTextColor
+import com.example.test_profileaccountdetails_compose.ui.theme.Rubik
+import com.example.test_profileaccountdetails_compose.ui.theme.SecondaryTextColor
+
+@Composable
+fun HeaderText(modifier: Modifier = Modifier, title: String, subTitle: String) {
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+
+        Text(
+            text = title,
+            color = PrimaryTextColor,
+            fontFamily = Rubik,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium
+
+        )
+
+        Text(
+            text = subTitle,
+            color = SecondaryTextColor,
+            fontFamily = Rubik,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Normal
+        )
+    }
+}
--- app/src/main/java/com/example/test_profileaccountdetails_compose/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/test_profileaccountdetails_compose/screens/ProfileScreen.kt
@@ -1,15 +1,43 @@
 package com.example.test_profileaccountdetails_compose.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.test_profileaccountdetails_compose.R
+import com.example.test_profileaccountdetails_compose.composable.HeaderText
+import com.example.test_profileaccountdetails_compose.composable.SpacerVertical32
 
 
 @Composable
 fun ProfileScreen() {
-    TODO("Not yet implemented")
-}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+        HeaderText(title = "Account", subTitle = "Edit and mange your account")
+
+        SpacerVertical32()
+
+        Image(
+            painter = painterResource(id = R.drawable.profile_img),
+            contentDescription = "profile picture",
+            modifier = Modifier.clip(shape = RoundedCornerShape(16.dp))
+        )
+
+    }
+}
 
 
 @Preview(showBackground = true)
