9e65efaaedce51aeb9e344a1b0fd78cc6e38900e
AmrNasser <amr931111@gmail.com>
2024-05-18 19:37:20 +0300

    add TextButton and SpacerVertical24

--- app/src/main/java/com/example/test_profileaccountdetails_compose/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/test_profileaccountdetails_compose/screens/ProfileScreen.kt
@@ -1,20 +1,20 @@
 package com.example.test_profileaccountdetails_compose.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.test_profileaccountdetails_compose.R
 import com.example.test_profileaccountdetails_compose.composable.HeaderText
+import com.example.test_profileaccountdetails_compose.composable.ProfileAvatar
+import com.example.test_profileaccountdetails_compose.composable.SpacerVertical24
 import com.example.test_profileaccountdetails_compose.composable.SpacerVertical32
+import com.example.test_profileaccountdetails_compose.composable.TextButton
 
 
 @Composable
@@ -30,11 +30,13 @@ fun ProfileScreen() {
 
         SpacerVertical32()
 
-        Image(
-            painter = painterResource(id = R.drawable.profile_img),
-            contentDescription = "profile picture",
-            modifier = Modifier.clip(shape = RoundedCornerShape(16.dp))
-        )
+        ProfileAvatar(painter = painterResource(R.drawable.profile_img))
+
+        SpacerVertical24()
+
+        TextButton(text = "Change profile picture"){}
+
+        SpacerVertical32()
 
     }
 }
